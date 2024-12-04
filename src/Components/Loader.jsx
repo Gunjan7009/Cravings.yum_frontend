22047b69c36e217fe432698a88ec9088ed52961a
Gunjan7009 <gunjandixit1712@gmail.com>
2024-12-05 01:13:57 +0530

    loadertrial

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Loader.css";
 
-export default Loader = ()=>{
+const Loader = ()=>{
     return (
         <div className="loader-overlay">
         <div className="loader"></div>
         </div>
     )
 }
+export default Loader;
